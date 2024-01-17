3d8f4d35403e8a794602dd6186ce3ebd22007d80
Quin Nordmark <quinnordmark@gmail.com>
2024-01-17 12:53:45 -0700

    unique_adult_passengers test

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -4,4 +4,23 @@ RSpec.describe Airline, type: :model do
   describe "relationships" do
     it {should have_many :flights}
   end
+
+  describe "model_functions" do
+    it "unique_adult_passengers" do
+      beth = Passenger.create(name: "Beth", age: 20)
+      fred = Passenger.create(name: "Fred", age: 8)
+
+      airline = Airline.create(name: "United")
+      flight_1 = airline.flights.create(number: "1")
+      flight_2 = airline.flights.create(number: "2")
+      PassengerFlight.create(passenger: beth, flight: flight_1)
+      PassengerFlight.create(passenger: beth, flight: flight_2)
+      PassengerFlight.create(passenger: fred, flight: flight_2)
+      
+      airline.unique_adult_passengers.each do |passenger|
+        expect(passenger.name).to eq("Beth")
+        expect(passenger.name).to_not eq("Fred")
+      end
+    end
+  end
 end
